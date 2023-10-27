995b978e9e7ac4f973a9f75d882ed13ccc9f88cc
azrazic <azricrazic75@gmail.com>
2023-10-27 22:38:30 +0200

    project added

---
+++ bacanjenovcica.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 20 20:22:23 2021
+
+@author: DT User2
+"""
+
+import numpy as np
+from numpy import random
+import matplotlib.pyplot as plt
+#rand
+
+def BacanjeNovcica (br):
+  i=0;
+  ishod=np.zeros(br, dtype=int)
+  while i<br:
+     ishod[i]=random.rand(1)<0.5
+     if ishod[i]==True:
+         ishod[i]=0;
+     else:
+         ishod[i]=1
+     i=i+1;
+  return ishod;
+
+A= BacanjeNovcica(1000)
+n1=np.count_nonzero(A==1)
+print(n1)
+p1=n1/1000;
+print(p1)
+print(A)
+plt.stem(A, use_line_collection=True)   
+plt.xlabel("Broj bacanja")
+plt.ylabel("Ishod eksperimenta")
+plt.grid()
+plt.tight_layout()
+     
---
+++ bacanjenovcica4puta.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 21 13:33:42 2021
+
+@author: DT User2
+"""
+
+import numpy as np
+from numpy import random
+import matplotlib.pyplot as plt
+#rand
+
+def BacanjeNovcica4puta (br):
+  i=0;
+  ishod=np.zeros(br, dtype=int)
+  while i<br:
+     j=0
+     pom=0
+     while j<4:
+         novcic=np.ceil(2*random.rand(1))
+         if novcic==1:
+             pom=pom+1
+         j=j+1
+     ishod[i]=pom
+     i=i+1;
+  return ishod;
+
+N=1000
+rezultat = BacanjeNovcica4puta (1000)
+n2=np.count_nonzero(rezultat==2)
+p2=n2/N
+plt.subplot (2,1,1)
+plt.stem(np.arange(1000),rezultat )
+plt.title ("Simulacija eksperimenta bacanja novcica")
+plt.xlabel ("N- broj bacanja ")
+plt.ylabel ("Ishod simulacije")
+plt.grid ()
+plt.tight_layout()
+
